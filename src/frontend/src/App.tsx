a41fd3af8816a4ee44bf45baad08bdfc2d1bb2a6
Lukas Bickel <lukas.bickel@consolidate.at>
2026-05-30 14:47:16 +0200

    lint

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { basePath } from "@/lib/basePath";
 import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
